ce28d0501b19158e4cf7e2ac502d67d230268683
Indresh Goswami <indreshgoswami149@gmail.com>
2023-05-26 02:13:59 +0530

    lottie added

--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -1,5 +1,6 @@
-import { StyleSheet, View,Text } from 'react-native';
-
+import { StyleSheet, View, Text, useWindowDimensions } from 'react-native';
+import { useState, useRef } from 'react';
+import Lottie from 'lottie-react-native'
 import { GlobalStyles } from '../../constants/styles';
 import ExpensesList from './ExpensesList';
 import ExpensesSummary from './ExpensesSummary';
@@ -12,10 +13,23 @@ function ExpensesOutput({ expenses, expensesPeriod ,fallbackText}) {
     if (expenses.length > 0) {
         content = <ExpensesList expenses={expenses} />;
     }
+
+    const { height } = useWindowDimensions();
+    const animationRef = useRef();
     return (
         <View style={styles.container}>
             <ExpensesSummary expenses={expenses} periodName={expensesPeriod} />
             {content}
+            <Lottie style={[styles.logo, { height: height * 0.3, marginLeft: 20 }]}
+                ref={animationRef}
+                //style = {{flex:1}}
+                source={require('../../assets/animations/58861-piggy-bank-coins-out.json')}
+                loop={true}
+                autoPlay={true}
+
+            //speed = {0.5}
+            />
+
         </View>
     );
 }
